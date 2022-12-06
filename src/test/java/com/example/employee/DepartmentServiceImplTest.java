79853b660a78afe2f28765542277860ce930ad0d
Aleksandr Gerb <aleksandrgerb87@gmail.com>
2022-12-07 00:29:23 +0300

    v3 @test DepartmentServiceImpl

--- src/test/java/com/example/employee/DepartmentServiceImplTest.java
+++ src/test/java/com/example/employee/DepartmentServiceImplTest.java
@@ -1,16 +1,28 @@
 package com.example.employee;
 
+import com.example.employee.model.Employee;
 import com.example.employee.record.EmployeeRequest;
 import com.example.employee.service.DepartmentServiceImpl;
 import com.example.employee.service.EmployeeServiceImpl;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
 
 import java.util.ArrayList;
-import java.util.List;
 
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+
+import static org.mockito.Mockito.when;
+
+@ExtendWith(MockitoExtension.class)
 public class DepartmentServiceImplTest {
+
     @Mock
     private EmployeeServiceImpl employeeService;
 
@@ -21,41 +33,81 @@ public class DepartmentServiceImplTest {
     private static EmployeeRequest employeeRequest1 = new EmployeeRequest();
     private static EmployeeRequest employeeRequest2 = new EmployeeRequest();
 
-    private List<EmployeeRequest> actualEmployees;
+    private List<Employee> actualEmployees;
+    private Integer department;
 
     @BeforeEach
-    public void setUp() {
-        //новый сотрудник
-        employeeRequest.setFirstName("Иван");
-        employeeRequest.setLastName("Иванов");
-        employeeRequest.setDepartment(1);
-        employeeRequest.setSalary(1000);
-        employeeRequest.setId(1);
-
-        //дублирование
-        employeeRequest1.setFirstName("Иван");
-        employeeRequest1.setLastName("Иванов");
-        employeeRequest1.setDepartment(1);
-        employeeRequest1.setSalary(2000);
-        employeeRequest1.setId(2);
-
-        //с маленькой буквы
-        employeeRequest2.setFirstName("иван");
-        employeeRequest2.setLastName("иванов");
-        employeeRequest2.setDepartment(2);
-        employeeRequest2.setSalary(3000);
-        employeeRequest2.setId(3);
+    public void  setUp() {
+        department = 1;
+        Employee employee = new Employee("Иван", "Иванов", 1, 1000);
+        Employee employee1 = new Employee("Иван1", "Иванов1", 1, 2000);
+        Employee employee2 = new Employee("Иван2", "Иванов2", 1, 3000);
+        Employee employee3 = new Employee("Иван2", "Иванов3", 2, 4000);
+        Employee employee4 = new Employee("Иван2", "Иванов4", 2, 5000);
 
         actualEmployees = new ArrayList<>(List.of(
-                employeeRequest,
-                employeeRequest1,
-                employeeRequest2));
+                employee,
+                employee1,
+                employee2,
+                employee3,
+                employee4
+        ));
+
+        when(employeeService.getAllEmployees()).thenReturn(actualEmployees);
 
     }
 
+    @Test
+    public void getAllEmployeesByDepartment() {
+        List<Employee> actual = actualEmployees.stream()
+                .filter(e->e.getDepartment() == department)
+                .toList();
+        List<Employee> expected = employeeService.getAllEmployees().stream()
+                .filter(e->e.getDepartment() == department)
+                .toList();
+        Assertions.assertEquals(expected, actual);
+    }
 
+    @Test
+    public void getSalarySumByDepartment() {
+        Integer actual = actualEmployees.stream()
+                .filter(e -> e.getDepartment() == department)
+                .mapToInt(t -> t.getSalary()).sum();
+        Integer expected = employeeService.getAllEmployees().stream()
+                .filter(e -> e.getDepartment() == department)
+                .mapToInt(t -> t.getSalary()).sum();
+        Assertions.assertEquals(expected, actual);
+    }
 
+    @Test
+    public void getSalaryMaxByDepartment() {
+        Integer actual = actualEmployees.stream()
+                .filter(e -> e.getDepartment() == department)
+                .mapToInt(t -> t.getSalary()).max().getAsInt();
+        Integer expected = employeeService.getAllEmployees().stream()
+                .filter(e -> e.getDepartment() == department)
+                .mapToInt(t -> t.getSalary()).max().getAsInt();
+        Assertions.assertEquals(expected, actual);
+    }
 
+    @Test
+    public void getSalaryMinByDepartment() {
+        Integer actual = actualEmployees.stream()
+                .filter(e -> e.getDepartment() == department)
+                .mapToInt(t -> t.getSalary()).min().getAsInt();
+        Integer expected = employeeService.getAllEmployees().stream()
+                .filter(e -> e.getDepartment() == department)
+                .mapToInt(t -> t.getSalary()).min().getAsInt();
+        Assertions.assertEquals(expected, actual);
+    }
 
+    @Test
+    public void getGroupEmployeesByDepartment() {
+        Map<Integer, List<Employee>> actual = actualEmployees.stream()
+                .collect(Collectors.groupingBy(Employee::getDepartment));
+        Map<Integer, List<Employee>> expected = employeeService.getAllEmployees().stream()
+                .collect(Collectors.groupingBy(Employee::getDepartment));
+        Assertions.assertEquals(expected, actual);
+    }
 
 }
